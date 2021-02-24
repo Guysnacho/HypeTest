d242acbc69f0da242edfc018972cfdb18a80a265
Samuel Adetunji <guysnacho2@gmail.com>
2021-02-23 20:30:20 -0600

    Added shoe form

--- src/routes/calculator.svelte
+++ src/routes/calculator.svelte
@@ -1,5 +1,6 @@
 <script>
     import Box from "../components/Box.svelte";
+    let shoe1 = undefined;
 </script>
 
 <style>
@@ -13,10 +14,22 @@
 		text-transform: uppercase;
 		font-weight: 300;
 	}
+
+    input {
+        border-radius: 10px;
+        border-style: hidden;
+        margin: auto;
+        align-content: center;
+    }
 </style>
 
 <h3>Input your 3 favorite sneaks</h3>
 
 <Box>
-    <input type="text" placeholder="Dusty Jordan 1's">
+    <p>No. 1</p>
+    <input type="text" placeholder="Dusty Jordan 1">
+    <p>No. 2</p>
+    <input type="text" placeholder="Nike Dunk Low Pine Green">
+    <p>No. 3</p>
+    <input type="text" placeholder="Odd Future Sk8 Hi">
 </Box>
